655884729494d345c13abaaa3aca6e20b940009e
Celia <celiaberon@gmail.com>
2024-02-06 18:31:54 -0500

    utility functions

---
+++ bh/utils.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def calc_sem(x):
+
+    return np.std(x) / np.sqrt(len(x))
+
+
+def calc_ci(x, confidence: float = 0.95):
+
+    from scipy.stats import norm
+
+    # determine critical value for set confidence interval.
+    alpha_2 = (1 - confidence) / 2
+    critical_value = norm.ppf(1 - alpha_2)
+
+    sem = calc_sem(x)
+    err_width = critical_value * sem
+    ci_low = np.mean(x) - err_width
+    ci_high = np.mean(x) + err_width
+    return (ci_low, ci_high, err_width)
