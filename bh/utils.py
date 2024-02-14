1bd304c16ad663ae3eee735642203c3fc4fd6510
Celia <celiaberon@gmail.com>
2024-02-14 14:05:55 -0500

    encoding sequences and dictionary idxing

--- bh/utils.py
+++ bh/utils.py
@@ -23,10 +23,36 @@ def calc_ci(x, confidence: float = 0.95):
 
 def make_onehot_array(x):
 
-    print(len(np.unique(x)))
     if len(np.unique(x)) == 1:
         return x
 
     onehot = np.zeros((x.size, x.max() + 1))
     onehot[np.arange(x.size), x] = 1
     return onehot
+
+
+def get_dict_item_by_idx(d, idx):
+
+    for i, (k, v) in enumerate(d.items()):
+        if i == idx:
+            return k, v
+        
+
+def encode_as_rl(choices, rewards):
+        
+    mapping = {(-1,0): 'r', (-1,1): 'R', (1,0): 'l', (1,1): 'L'} 
+
+    return ''.join([mapping[(c,r)] for c,r in zip(choices, rewards)])
+
+def encode_sequences(rlseq, lag=3):
+
+    mappings_ref_L = {'L': 'A', 'l': 'a', 'R': 'B', 'r': 'b'}
+    mappings_ref_R = {'R': 'A', 'r': 'a', 'L': 'B', 'l': 'b'}
+
+    seqs = []
+    for first, second, third in zip(rlseq[2:], rlseq[1:-1], rlseq[:-2]):
+
+        ref = mappings_ref_L if first.upper()=='L' else mappings_ref_R
+        seqs.append(''.join([ref[first], ref[second], ref[third]]))
+
+    return seqs
